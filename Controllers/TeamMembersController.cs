f03a177b8f921c56c01c0b126bb12aecab23a9ac
Kevin Mangold <kmangold96@gmail.com>
2024-12-01 15:24:08 -0500

    base controller

---
+++ Controllers/TeamMembersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Final_Project.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeamMembersController : Controller
+    {
+        private readonly ILogger<TeamMembersController> _logger;
+        public TeamMembersController(ILogger<TeamMembersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
