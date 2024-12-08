35e851d59fd5c9774188eab434ba8f620604ba8e
Kevin Mangold <kmangold96@gmail.com>
2024-12-07 21:11:18 -0500

    databaseupdate

--- obj/Debug/net8.0/Final Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Final Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+809d7b72d90e0c9e793e095e1ee71a69d5fcabce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd00cdc4dc0eb42701a4d20c09b4863aae60231a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
